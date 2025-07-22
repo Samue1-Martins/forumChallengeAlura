f30ed2255102d86e6b28bc4752e696b1b3c161a3
Samuel Douglas M.M <samuel30douglas@gmail.com>
2025-07-22 11:58:44 -0300

    Initial commit

---
+++ src/main/java/br/com/alura/forumChallengeAlura/ForumChallengeAluraApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.forumChallengeAlura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumChallengeAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForumChallengeAluraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/forumChallengeAlura/ForumChallengeAluraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.forumChallengeAlura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumChallengeAluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
