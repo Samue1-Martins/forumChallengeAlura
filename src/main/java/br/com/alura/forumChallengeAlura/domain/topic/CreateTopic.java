20405e6a246f8527c864c00b774d9add5cacd62a
Samuel Douglas M.M <samuel30douglas@gmail.com>
2025-07-23 21:40:59 -0300

    feat: creatig record class topics, for show data on return method post and validation

--- src/main/java/br/com/alura/forumChallengeAlura/domain/topic/CreateTopic.java
+++ src/main/java/br/com/alura/forumChallengeAlura/domain/topic/CreateTopic.java
@@ -1,4 +1,24 @@
 package br.com.alura.forumChallengeAlura.domain.topic;
 
-public record CreateTopic() {
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.validator.constraints.NotBlank;
+
+import java.util.Date;
+
+public record CreateTopic(
+        @NotBlank
+        String title,
+        @NotBlank
+        String message,
+        Date createdAt,
+        @NotBlank
+        String status,
+        @NotBlank
+        String author,
+        @NotBlank
+        String response,
+        @NotNull
+        CourseEnum course
+        ) {
+
 }
