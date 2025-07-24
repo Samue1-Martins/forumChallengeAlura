6e1401754b33579978ff99189ba5c3bbc9860cda
Samuel Douglas M.M <samuel30douglas@gmail.com>
2025-07-23 21:44:17 -0300

    feat: class for show data retunr in method post on return api

--- src/main/java/br/com/alura/forumChallengeAlura/domain/topic/DataTopicDetails.java
+++ src/main/java/br/com/alura/forumChallengeAlura/domain/topic/DataTopicDetails.java
@@ -1,4 +1,23 @@
 package br.com.alura.forumChallengeAlura.domain.topic;
 
-public record DataTopicDetails() {
+import java.util.Date;
+
+public record DataTopicDetails(
+         Long id,
+         String title,
+         String message,
+         Date createdAt,
+         String status,
+         String author,
+         String response
+) {
+    public DataTopicDetails(TopicClass topic){
+    this(topic.getId(),
+            topic.getTitle(),
+            topic.getAuthor(),
+            topic.getCreatedAt(),
+            topic.getAuthor(),
+            topic.getMessage(),
+            topic.getResponse());
+    }
 }
