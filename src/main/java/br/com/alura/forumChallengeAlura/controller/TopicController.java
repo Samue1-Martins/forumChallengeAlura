d2040bf4cfeed6b1a44577e00848aa2972967571
Samuel Douglas M.M <samuel30douglas@gmail.com>
2025-07-24 17:16:50 -0300

    fix: adding method get topic with id

--- src/main/java/br/com/alura/forumChallengeAlura/controller/TopicController.java
+++ src/main/java/br/com/alura/forumChallengeAlura/controller/TopicController.java
@@ -4,6 +4,9 @@ import br.com.alura.forumChallengeAlura.domain.topic.*;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -33,5 +36,11 @@ public class TopicController {
         var page = repository.findAll();
         return ResponseEntity.ok(page);
     }
+    @GetMapping("/{id}")
+    public ResponseEntity detailTopicWithId(@PathVariable Long id){
+        var topic = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DataTopicDetails(topic));
+    }
+
 
 }
