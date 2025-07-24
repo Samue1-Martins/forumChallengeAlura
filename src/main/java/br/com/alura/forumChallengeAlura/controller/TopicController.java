706bb42f05d585e88a4c70db163e28ff27449c00
Samuel Douglas M.M <samuel30douglas@gmail.com>
2025-07-23 21:37:43 -0300

    feat: adding controller for topics, creating routes get and post

--- src/main/java/br/com/alura/forumChallengeAlura/controller/TopicController.java
+++ src/main/java/br/com/alura/forumChallengeAlura/controller/TopicController.java
@@ -1,4 +1,35 @@
 package br.com.alura.forumChallengeAlura.controller;
 
+import br.com.alura.forumChallengeAlura.domain.topic.*;
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("topic")
 public class TopicController {
+
+    @Autowired
+    private TopicRepository repository;
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity createTopic(@RequestBody @Valid CreateTopic data, UriComponentsBuilder uriBuilder){
+        var topic = new TopicClass(data);
+        repository.save(topic);
+        var uri = uriBuilder.path("/topics/{id}").buildAndExpand(topic.getId()).toUri();
+        return ResponseEntity.created(uri).body(new DataTopicDetails(topic));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TopicClass>> list(){
+        var page = repository.findAll();
+        return ResponseEntity.ok(page);
+    }
+
 }
