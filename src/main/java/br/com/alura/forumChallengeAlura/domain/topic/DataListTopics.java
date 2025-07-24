fe6fe9301c4e1c4794020e54ba99069d734aa143
Samuel Douglas M.M <samuel30douglas@gmail.com>
2025-07-23 21:43:12 -0300

    feat: adding record for get data in db and make pageable

--- src/main/java/br/com/alura/forumChallengeAlura/domain/topic/DataListTopics.java
+++ src/main/java/br/com/alura/forumChallengeAlura/domain/topic/DataListTopics.java
@@ -1,4 +1,23 @@
 package br.com.alura.forumChallengeAlura.domain.topic;
 
-public record DataListTopics() {
+import java.util.Date;
+
+public record DataListTopics(
+        Long id,
+        String title,
+        String message,
+        Date createdAt,
+        String status,
+        String author,
+        String response
+) {
+    public DataListTopics(TopicClass listTopic){
+        this(listTopic.getId(),
+                listTopic.getTitle(),
+                listTopic.getAuthor(),
+                listTopic.getCreatedAt(),
+                listTopic.getAuthor(),
+                listTopic.getMessage(),
+                listTopic.getResponse());
+    }
 }
