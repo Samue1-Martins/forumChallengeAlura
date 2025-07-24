178c5e46b9a207a3b0e81d64e1debd26333c340a
Samuel Douglas M.M <samuel30douglas@gmail.com>
2025-07-23 21:36:07 -0300

    fix: commenting dependency security for init project

---
+++ src/main/java/br/com/alura/forumChallengeAlura/controller/TopicController.java
@@ -0,0 +1,4 @@
+package br.com.alura.forumChallengeAlura.controller;
+
+public class TopicController {
+}
---
+++ src/main/java/br/com/alura/forumChallengeAlura/domain/topic/TopicRepository.java
@@ -0,0 +1,4 @@
+package br.com.alura.forumChallengeAlura.domain.topic;
+
+public class TopicRepository {
+}
