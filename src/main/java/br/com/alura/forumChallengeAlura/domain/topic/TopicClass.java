ef52be38fdb6aabf9e6bb7f73c540206e6b4a735
Samuel Douglas M.M <samuel30douglas@gmail.com>
2025-07-23 21:45:28 -0300

    feat: creating atributes on class topic

--- src/main/java/br/com/alura/forumChallengeAlura/domain/topic/TopicClass.java
+++ src/main/java/br/com/alura/forumChallengeAlura/domain/topic/TopicClass.java
@@ -1,4 +1,42 @@
 package br.com.alura.forumChallengeAlura.domain.topic;
 
+import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Table(name = "topics")
+@Entity(name = "Topic")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class TopicClass {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String message;
+    private Date createdAt;
+    private String status;
+    private String author;
+    private String response;
+
+    @Enumerated(EnumType.STRING)
+    private CourseEnum course;
+
+    public TopicClass(CreateTopic data) {
+        this.title = data.title();
+        this.message = data.message();
+        this.createdAt = data.createdAt();
+        this.status = data.status();
+        this.author = data.author();
+        this.response = data.response();
+        this.course = data.course();
+    }
 }
