8ad8f38756c8130f1e68e745e332184f61ce97b8
Samuel Douglas M.M <samuel30douglas@gmail.com>
2025-08-06 13:53:33 -0300

    feat: implementing class service

--- src/main/java/br/com/alura/forumChallengeAlura/domain/topic/TopicService.java
+++ src/main/java/br/com/alura/forumChallengeAlura/domain/topic/TopicService.java
@@ -1,10 +1,12 @@
 package br.com.alura.forumChallengeAlura.domain.topic;
 
-import br.com.alura.forumChallengeAlura.domain.users.UserRepository;
-import br.com.alura.forumChallengeAlura.domain.users.UsersClass;
+import br.com.alura.forumChallengeAlura.domain.users.*;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TopicService {
@@ -16,14 +18,45 @@ public class TopicService {
     private UserRepository userRepository;
 
     @Transactional
-    public TopicClass createTopic(CreateTopic data, Long userId){
-
+    public TopicClass createTopic(DataCreateTopic data, Long userId){
         UsersClass authorUser = userRepository.findById(userId)
-                .orElseThrow(() -> new IllegalArgumentException("Usuário não encontrado com o ID: " + userId));
+                .orElseThrow(() -> new IllegalArgumentException("topic not found. " + userId));
         TopicClass newTopic = new TopicClass(data, authorUser);
         topicRepository.save(newTopic);
-
         return newTopic;
     }
 
+    @Transactional
+    public List<DataTopicDetails> listTopics(){
+        var ListTopic = topicRepository
+                .findAll()
+                .stream()
+                .map(DataTopicDetails::new)
+                .toList();
+        return ListTopic;
+    }
+
+    @Transactional
+    public DataTopicDetails detailTopicId(Long id){
+        TopicClass topic = topicRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("topic not found."));
+        var topicDetails = new DataTopicDetails(topic);
+        return topicDetails;
+    }
+
+    @Transactional
+    public TopicClass updateUser(Long id, DataUpdateTopic data){
+        TopicClass topic = topicRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("topic not found."));
+        topic.updateTopics(data);
+        return topicRepository.save(topic);
+    }
+
+    @Transactional
+    public void deleteTopic(Long id){
+        if (!topicRepository.existsById(id)){
+            throw new EntityNotFoundException("Topic não encontrado.");
+        }
+        topicRepository.deleteById(id);
+    }
 }
--- src/main/java/br/com/alura/forumChallengeAlura/domain/users/UserService.java
+++ src/main/java/br/com/alura/forumChallengeAlura/domain/users/UserService.java
@@ -1,4 +1,60 @@
 package br.com.alura.forumChallengeAlura.domain.users;
 
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Transactional
+    public UsersClass registerUser(DataCreateUsers data){
+        var hashedPassword = passwordEncoder.encode(data.password());
+        UsersClass newUser = new UsersClass(data.name(), data.email(), hashedPassword);
+        return userRepository.save(newUser);
+    }
+
+    @Transactional
+    public UsersClass updateUser(Long id, DataUpdateUser data){
+        UsersClass user = userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("User not found."));
+        user.updateUser(data, passwordEncoder);
+        return userRepository.save(user);
+    }
+
+    @Transactional
+    public List<DataUserDetails> listAllUser(){
+         var user = userRepository
+                .findAll()
+                .stream()
+                .map(DataUserDetails::new)
+                .toList();
+        return user;
+    }
+
+    @Transactional
+    public DataUserTopicsDetails detailUserId(Long id){
+        UsersClass user = userRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("User not found."));
+        var userDetails = new DataUserTopicsDetails(user);
+        return userDetails;
+    }
+
+    @Transactional
+    public void deleteUser(Long id){
+        if (!userRepository.existsById(id)){
+            throw new EntityNotFoundException("Usuário não encontrado.");
+        }
+        userRepository.deleteById(id);
+    }
 }
