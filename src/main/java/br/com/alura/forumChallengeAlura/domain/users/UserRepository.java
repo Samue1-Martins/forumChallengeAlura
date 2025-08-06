b45bd8148fa6ce4d69856a4c40906aa95709748c
Samuel Douglas M.M <samuel30douglas@gmail.com>
2025-08-05 21:32:03 -0300

    feat: adding method find user by email

--- src/main/java/br/com/alura/forumChallengeAlura/domain/users/UserRepository.java
+++ src/main/java/br/com/alura/forumChallengeAlura/domain/users/UserRepository.java
@@ -1,9 +1,11 @@
 package br.com.alura.forumChallengeAlura.domain.users;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UsersClass, Long> {
     Optional<UsersClass> findById(Long id);
+    Optional<UserDetails> findByEmail(String email);
 }
